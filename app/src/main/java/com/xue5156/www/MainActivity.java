d6c91cb3f6e9eb264653dea9c40ce25bb6663b7e
xiaokangkang168lxk <401806499@qq.com>
2020-08-03 17:13:41 +0800

    首次提交

---
+++ app/src/main/java/com/xue5156/www/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xue5156.www;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
